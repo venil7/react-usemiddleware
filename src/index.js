2fee0a86c1275513b8c76ebd3f89df7e4f092c7c
Art Deineka <art.deineka@darkruby.co.uk>
2018-11-30 14:16:25 +0000

    added code and docs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { applyMiddleware } from 'redux';
+
+const useMiddleware = (middlewares, reducer, initState) => {
+  const [state, dispatch] = useReducer(reducer, initState);
+  const getStore = () => ({ dispatch, getState: () => state });
+  const store = applyMiddleware(...middlewares)(getStore)();
+  const { dispatch: dispatch_, getState } = store;
+  return [getState(), dispatch_];
+};
+
+export default useMiddleware;
